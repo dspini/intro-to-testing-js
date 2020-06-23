745b55cf792f09ad0713d4293bb874b7582e54ac
Donald Spini <donald.spini68@gmail.com>
2020-06-23 15:06:47 -0500

    first test solution intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,13 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+// function expression syntax (assigning an anonymous function to a variable)
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function sayHello() {
+    return "Hello";
+}
+
